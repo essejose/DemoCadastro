16e832cdf1d5028cd468ed1b1e3a34a7d53a2548
essejose <ze_pk2@hotmail.com>
2017-06-07 23:00:47 -0300

    BroadcastRecive

--- app/src/main/java/com/example/logonrm/democadastro/LoginActivity.java
+++ app/src/main/java/com/example/logonrm/democadastro/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.logonrm.democadastro;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -34,6 +38,8 @@ public class LoginActivity extends AppCompatActivity {
                     .setText(savedInstanceState.getString("TEXTO"));
         }
 
+        requestSmsPermission();
+
 
     }
 
@@ -55,5 +61,16 @@ public class LoginActivity extends AppCompatActivity {
                 break;
         }
     }
+
+    private void requestSmsPermission() {
+        String permission = Manifest.permission.RECEIVE_SMS;
+        int grant = ContextCompat.checkSelfPermission(this, permission);
+        if ( grant != PackageManager.PERMISSION_GRANTED) {
+            String[] permission_list = new String[1];
+            permission_list[0] = permission;
+            ActivityCompat.requestPermissions(this, permission_list, 1);
+        }
+    }
+
 
 }
